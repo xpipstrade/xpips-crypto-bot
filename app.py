84a1424bd36c5f54b7e8f7bf6e90d312fc43479b
xpipstrade <finweyoo@gmail.com>
2025-05-05 12:06:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 TOKEN = "7893984645:AAHXUAU0nScgo4MB18zjCWeg6W28Kgvfqdc"
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
+@app.route("/")
+def home():
+    return "Bot XPIPS en ligne !"
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
@@ -16,9 +20,6 @@ def webhook():
     if chat_id and message:
         send_message(chat_id, f"Hello from the bot: {message}")
     return {'status': 'ok'}, 200
-def home():
-    return "Bot XPIPS en ligne !"
-
 
 def send_message(chat_id, text):
     params = {'chat_id': chat_id, 'text': text}
